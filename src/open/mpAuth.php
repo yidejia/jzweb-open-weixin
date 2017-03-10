57f28c14c790e3be69756b18cc277357081a135d
liusongsen <liusongsen@gmail.com>
2017-03-10 12:34:36 +0800

    集成微信开放平台加解密函数 并转化为命名空间 官方给的库版本太低了 升级一些

---
+++ src/open/mpAuth.php
@@ -0,0 +1,24 @@
+<?php
+namespace jzweb\open\weixin\open;
+
+
+/**
+ * 公众号第三方平台-微信用户的一键授权功能
+ *
+ * @user 刘松森 <liusongsen@gmail.com>
+ * @date 2017/3/10
+ */
+class mpAuth
+{
+
+
+    # 第三方平台获取预授权码(pre_auth_code)
+
+    # 引用用户进入授权页
+
+    # 用户确认并同意登录授权第三方平台方
+
+    # 授权后回掉URL,得到授权码(authoriaztion_code)和过期时间
+
+    # 利用授权码调用用户公众号相关API
+}
