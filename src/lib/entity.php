2196affecb6c81ec2509de2306a11ae3355c58a5
liusongsen <liusongsen@gmail.com>
2017-03-11 12:31:40 +0800

    修复msg_type命名bug

--- src/lib/entity.php
+++ src/lib/entity.php
@@ -32,19 +32,19 @@ class entity
         }
         switch ($data['InfoType']) {
             case "unauthorized";
-                $result = array('msgType' => "unauthorized", 'data' => unauthorized::toArray($data));
+                $result = array('msg_type' => "unauthorized", 'data' => unauthorized::toArray($data));
                 break;
             case "authorized";
-                $result = array('msgType' => "authorized", 'data' => authorized::toArray($data));
+                $result = array('msg_type' => "authorized", 'data' => authorized::toArray($data));
                 break;
             case "updateauthorized";
-                $result = array('msgType' => "updateauthorized", 'data' => updateauthorized::toArray($data));
+                $result = array('msg_type' => "updateauthorized", 'data' => updateauthorized::toArray($data));
                 break;
             case "component_verify_ticket";
-                $result = array('msgType' => "component_verify_ticket", 'data' => ticket::toArray($data));
+                $result = array('msg_type' => "component_verify_ticket", 'data' => ticket::toArray($data));
                 break;
             default:
-                $result = array('msgType' => "unknown", 'data' => $data);
+                $result = array('msg_type' => "unknown", 'data' => $data);
                 break;
         }
         return $result;
