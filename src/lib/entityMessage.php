5f083ad3b55dad17b37a9d175351f57a728ab881
liusongsen <liusongsen@gmail.com>
2017-03-15 10:44:52 +0800

    统一消息实体格式 增加类型区分

--- src/lib/entityMessage.php
+++ src/lib/entityMessage.php
@@ -1,6 +1,12 @@
 <?php
 namespace jzweb\open\weixin\lib;
 
+use jzweb\open\weixin\lib\entity\event\clickEvent;
+use jzweb\open\weixin\lib\entity\event\locationEvent;
+use jzweb\open\weixin\lib\entity\event\scanEvent;
+use jzweb\open\weixin\lib\entity\event\subscribeEvent;
+use jzweb\open\weixin\lib\entity\event\unsubscribeEvent;
+use jzweb\open\weixin\lib\entity\event\viewEvent;
 use jzweb\open\weixin\lib\entity\message\linkMsg;
 use jzweb\open\weixin\lib\entity\message\locationMsg;
 use jzweb\open\weixin\lib\entity\message\picMsg;
@@ -33,32 +39,58 @@ class entityMessage
         foreach ($obj as $key => $value) {
             $data[$key] = strval($value);
         }
-
-        switch ($data['MsgType']) {
-            case "text";
-                $result = array('msg_type' => "text", 'data' => textMsg::toArray($data));
-                break;
-            case "image";
-                $result = array('msg_type' => "image", 'data' => picMsg::toArray($data));
-                break;
-            case "voice";
-                $result = array('msg_type' => "voice", 'data' => voiceMsg::toArray($data));
-                break;
-            case "video";
-                $result = array('msg_type' => "video", 'data' => videoMsg::toArray($data));
-                break;
-            case "shortvideo":
-                $result = array('msg_type' => "shortvideo", 'data' => shortvideoMsg::toArray($data));
-                break;
-            case "location":
-                $result = array('msg_type' => "location", 'data' => locationMsg::toArray($data));
-                break;
-            case "link":
-                $result = array('msg_type' => "link", 'data' => linkMsg::toArray($data));
-                break;
-            default:
-                $result = array('msg_type' => "unknown", 'data' => $data);
-                break;
+        //区分消息类型【事件+消息】
+        if ($data['MsgType'] == "event" && isset($data['Event'])) {
+            switch ($data['Event']) {
+                case "subscribe";
+                    $result = array('msg_type' => "subscribe", 'type' => 'event', 'data' => subscribeEvent::toArray($data));
+                    break;
+                case "unsubscribe";
+                    $result = array('msg_type' => "unsubscribe", 'type' => 'event', 'data' => unsubscribeEvent::toArray($data));
+                    break;
+                case "SCAN";
+                    $result = array('msg_type' => "scan", 'type' => 'event', 'data' => scanEvent::toArray($data));
+                    break;
+                case "LOCATION";
+                    $result = array('msg_type' => "location", 'type' => 'event', 'data' => locationEvent::toArray($data));
+                    break;
+                case "CLICK":
+                    $result = array('msg_type' => "click", 'type' => 'event', 'data' => clickEvent::toArray($data));
+                    break;
+                case "VIEW":
+                    $result = array('msg_type' => "view", 'type' => 'event', 'data' => viewEvent::toArray($data));
+                    break;
+                default:
+                    $result = array('msg_type' => "unknown", 'type' => 'event', 'data' => $data);
+                    break;
+            }
+        } else {
+            switch ($data['MsgType']) {
+                case "text";
+                    $result = array('msg_type' => "text", 'type' => 'message', 'data' => textMsg::toArray($data));
+                    break;
+                case "image";
+                    $result = array('msg_type' => "image", 'type' => 'message', 'data' => picMsg::toArray($data));
+                    break;
+                case "voice";
+                    $result = array('msg_type' => "voice", 'type' => 'message', 'data' => voiceMsg::toArray($data));
+                    break;
+                case "video";
+                    $result = array('msg_type' => "video", 'type' => 'message', 'data' => videoMsg::toArray($data));
+                    break;
+                case "shortvideo":
+                    $result = array('msg_type' => "shortvideo", 'type' => 'message', 'data' => shortvideoMsg::toArray($data));
+                    break;
+                case "location":
+                    $result = array('msg_type' => "location", 'type' => 'message', 'data' => locationMsg::toArray($data));
+                    break;
+                case "link":
+                    $result = array('msg_type' => "link", 'type' => 'message', 'data' => linkMsg::toArray($data));
+                    break;
+                default:
+                    $result = array('msg_type' => "unknown", 'type' => 'message', 'data' => $data);
+                    break;
+            }
         }
         return $result;
     }
