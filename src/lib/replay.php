f439d7d51f36090c682feae5e581f318537405cf
liusongsen <liusongsen@gmail.com>
2017-03-29 14:22:28 +0800

    升级接口 统一抽象消息回复

--- src/lib/replay.php
+++ src/lib/replay.php
@@ -17,13 +17,13 @@ class replay
      * @param string $to_user 接收方微信用户openid
      * @param string $from_user 开发者微信号
      * @param int $hte_time 消息发送时间
-     * @param string $content 消息内容
+     * @param string $data 消息内容
      *
      * @return string
      */
-    public static function replayText($to_user, $from_user, $the_time, $content)
+    public static function replayText($to_user, $from_user, $the_time, $data)
     {
-        return "<xml><ToUserName><![CDATA[" . $to_user . "]]></ToUserName><FromUserName><![CDATA[" . $from_user . "]]></FromUserName><CreateTime>" . $the_time . "</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[" . $content . "]]></Content></xml>";
+        return "<xml><ToUserName><![CDATA[" . $to_user . "]]></ToUserName><FromUserName><![CDATA[" . $from_user . "]]></FromUserName><CreateTime>" . $the_time . "</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[" . $data . "]]></Content></xml>";
     }
 
     /**
@@ -32,13 +32,13 @@ class replay
      * @param string $to_user 接收方微信用户openid
      * @param string $from_user 开发者微信号
      * @param int $the_time 消息发送时间
-     * @param string $media_id 通过素材管理中的接口上传多媒体文件，得到的id。
+     * @param string $data 通过素材管理中的接口上传多媒体文件，得到的id。
      *
      * @return string
      */
-    public static function replayImage($to_user, $from_user, $the_time, $media_id)
+    public static function replayImage($to_user, $from_user, $the_time, $data)
     {
-        return "<xml><ToUserName><![CDATA[" . $to_user . "]]></ToUserName><FromUserName><![CDATA[" . $from_user . "]]></FromUserName><CreateTime>" . $the_time . "</CreateTime><MsgType><![CDATA[image]]></MsgType><Image><MediaId><![CDATA[" . $media_id . "]]></MediaId></Image></xml>";
+        return "<xml><ToUserName><![CDATA[" . $to_user . "]]></ToUserName><FromUserName><![CDATA[" . $from_user . "]]></FromUserName><CreateTime>" . $the_time . "</CreateTime><MsgType><![CDATA[image]]></MsgType><Image><MediaId><![CDATA[" . $data . "]]></MediaId></Image></xml>";
     }
 
     /**
@@ -47,13 +47,13 @@ class replay
      * @param string $to_user 接收方微信用户openid
      * @param string $from_user 开发者微信号
      * @param int $the_time 消息发送时间
-     * @param string $media_id 通过素材管理中的接口上传多媒体文件，得到的id。
+     * @param string $data 通过素材管理中的接口上传多媒体文件，得到的id。
      *
      * @return string
      */
-    public static function replayVoice($to_user, $from_user, $the_time, $media_id)
+    public static function replayVoice($to_user, $from_user, $the_time, $data)
     {
-        return "<xml><ToUserName><![CDATA[" . $to_user . "]]></ToUserName><FromUserName><![CDATA[" . $from_user . "]]></FromUserName><CreateTime>" . $the_time . "</CreateTime><MsgType><![CDATA[voice]]></MsgType><Voice><MediaId><![CDATA[" . $media_id . "]]></MediaId></Voice></xml>";
+        return "<xml><ToUserName><![CDATA[" . $to_user . "]]></ToUserName><FromUserName><![CDATA[" . $from_user . "]]></FromUserName><CreateTime>" . $the_time . "</CreateTime><MsgType><![CDATA[voice]]></MsgType><Voice><MediaId><![CDATA[" . $data . "]]></MediaId></Voice></xml>";
     }
 
 
@@ -63,15 +63,13 @@ class replay
      * @param string $to_user 接收方微信用户openid
      * @param string $from_user 开发者微信号
      * @param int $the_time 消息发送时间
-     * @param string $media_id 通过素材管理中的接口上传多媒体文件，得到的id。
-     * @param string $title 视频消息标题
-     * @param string $description 视频消息的描述
+     * @param array $data 回复内容（media_id|title|description) 通过素材管理中的接口上传多媒体文件，得到的id。
      *
      * @return string
      */
-    public static function replayVideo($to_user, $from_user, $the_time, $media_id, $title, $description)
+    public static function replayVideo($to_user, $from_user, $the_time, $data)
     {
-        return "<xml><ToUserName><![CDATA[" . $to_user . "]]></ToUserName><FromUserName><![CDATA[" . $from_user . "]]></FromUserName><CreateTime>" . $the_time . "</CreateTime><MsgType><![CDATA[video]]></MsgType><Video><MediaId><![CDATA[" . $media_id . "]]></MediaId><Title><![CDATA[" . $title . "]]></Title><Description><![CDATA[" . $description . "]]></Description></Video></xml>";
+        return "<xml><ToUserName><![CDATA[" . $to_user . "]]></ToUserName><FromUserName><![CDATA[" . $from_user . "]]></FromUserName><CreateTime>" . $the_time . "</CreateTime><MsgType><![CDATA[video]]></MsgType><Video><MediaId><![CDATA[" . $data['media_id'] . "]]></MediaId><Title><![CDATA[" . $data['title'] . "]]></Title><Description><![CDATA[" . $data['description'] . "]]></Description></Video></xml>";
     }
 
 
@@ -81,17 +79,13 @@ class replay
      * @param string $to_user 接收方微信用户openid
      * @param string $from_user 开发者微信号
      * @param int $the_time 消息发送时间
-     * @param string $title 音乐标题
-     * @param string $description 音乐描述
-     * @param string $music_url 音乐链接
-     * @param string $hq_music_url 高质量音乐链接，WIFI环境优先使用该链接播放音乐
-     * @param string $thumb_media_id 缩略图的媒体id，通过素材管理中的接口上传多媒体文件，得到的id
+     * @param array $data 回复内容(title|description|music_url|hq_music_url|thumb_media_id)
      *
      * @return string
      */
-    public static function replayMusic($to_user, $from_user, $the_time, $title, $description, $music_url, $hq_music_url, $thumb_media_id)
+    public static function replayMusic($to_user, $from_user, $the_time, $data)
     {
-        return "<xml><ToUserName><![CDATA[" . $to_user . "]]></ToUserName><FromUserName><![CDATA[" . $from_user . "]]></FromUserName><CreateTime>" . $the_time . "</CreateTime><MsgType><![CDATA[music]]></MsgType><Music><Title><![CDATA[" . $title . "]]></Title><Description><![CDATA[" . $description . "]]></Description><MusicUrl><![CDATA[" . $music_url . "]]></MusicUrl><HQMusicUrl><![CDATA[" . $hq_music_url . "]]></HQMusicUrl><ThumbMediaId><![CDATA[" . $thumb_media_id . "]]></ThumbMediaId></Music></xml>";
+        return "<xml><ToUserName><![CDATA[" . $to_user . "]]></ToUserName><FromUserName><![CDATA[" . $from_user . "]]></FromUserName><CreateTime>" . $the_time . "</CreateTime><MsgType><![CDATA[music]]></MsgType><Music><Title><![CDATA[" . $data['title'] . "]]></Title><Description><![CDATA[" . $data['description'] . "]]></Description><MusicUrl><![CDATA[" . $data['music_url'] . "]]></MusicUrl><HQMusicUrl><![CDATA[" . $data['hq_music_url'] . "]]></HQMusicUrl><ThumbMediaId><![CDATA[" . $data['thumb_media_id'] . "]]></ThumbMediaId></Music></xml>";
     }
 
 
@@ -100,19 +94,19 @@ class replay
      *
      * @param string $to_user 接收方微信用户openid
      * @param string $from_user 开发者微信号
-     * @param int $the_time  消息发送时间
-     * @param array $articles 注意多条图文格式[title|description|pic_url|url]
+     * @param int $the_time 消息发送时间
+     * @param array $data 注意多条图文格式[title|description|pic_url|url]
      *
      * @return string
      */
-    public static function replayNews($to_user, $from_user, $the_time, $articles)
+    public static function replayNews($to_user, $from_user, $the_time, $data)
     {
 
         $str = "";
-        foreach ($articles as $article) {
+        foreach ($data as $article) {
             $str .= "<item><Title><![CDATA[" . $article['title'] . "]]></Title><Description><![CDATA[" . $article['description'] . "]]></Description><PicUrl><![CDATA[" . $article['pic_url'] . "]]></PicUrl><Url><![CDATA[" . $article['url'] . "]]></Url></item>";
         }
-        return "<xml><ToUserName><![CDATA[" . $to_user . "]]></ToUserName><FromUserName><![CDATA[" . $from_user . "]]></FromUserName><CreateTime>" . $the_time . "</CreateTime><MsgType><![CDATA[news]]></MsgType><ArticleCount>" . count($articles) . "</ArticleCount><Articles>" . $str . "</Articles></xml>";
+        return "<xml><ToUserName><![CDATA[" . $to_user . "]]></ToUserName><FromUserName><![CDATA[" . $from_user . "]]></FromUserName><CreateTime>" . $the_time . "</CreateTime><MsgType><![CDATA[news]]></MsgType><ArticleCount>" . count($data) . "</ArticleCount><Articles>" . $str . "</Articles></xml>";
     }
 
 
